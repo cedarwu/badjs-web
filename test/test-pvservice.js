b73bd07ef59579718a1cf388e869897ac4a57ee7
donaldcen <donaldcen@tencent.com>
2015-11-01 18:16:50 +0800

    test pv

---
+++ test/test-pvservice.js
@@ -0,0 +1,14 @@
+var PvService = require('../service/PvService');
+var FileStorage = require('../service/FileStorage');
+
+var pvService = PvService.create({
+    filePath: '../fileStorage/',
+    filePrefix: 'pv_',
+    fileSuffix: ''
+});
+
+PvService.getPVByDate('201510302220', function (err, data) {
+    pvService.save(data, function (err, data) {
+        console.log(err, data);
+    });
+});
